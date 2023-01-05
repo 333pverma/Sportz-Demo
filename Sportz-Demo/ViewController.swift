d649a0e32a23cc3986045df919e7b1d0806300e4
Pramod <pramod@nickelfox.com>
2023-01-05 21:13:20 +0530

    Initial Commit

---
+++ Sportz-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sportz-Demo
+//
+//  Created by Pramod on 05/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
